24c4989538a0d730292b747ce3a356aa2b7e6e6f
aayushananda <aayushanand300@gmail.com>
2026-03-25 07:15:25 +0530

    used yargs to make custom cli scripting organized and used fs module for making changes in the note

---
+++ src/command.js
@@ -0,0 +1,64 @@
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+import {newNote, getAllNotes, findNotes} from './notes.js'
+import {listNotes} from './utils.js'
+
+yargs(hideBin(process.argv))
+  .command('new <note>', 'Create a new note', yargs=>{
+    return yargs.positional('note', {
+      type: 'string',
+      description : 'The content of the note to create',
+    })
+  }, async (argv)=>{
+    const tags = argv.tags  ? argv.tags.split(',') : []
+    const note = await newNote(argv.note , tags)
+    console.log('New note!' , note)
+    })
+
+  .options('tags', {
+    alias: 't',
+    type: 'string',
+    description: 'tags to add to the note'
+  })
+
+
+  .command('all', 'get all notes', ()=>{}, async(argv)=>{
+    const notes = await getAllNotes()
+    listNotes(notes)
+
+  })
+
+  .command('find <filter>', 'get matching notes', yargs=>{
+    return yargs.positional('filter', {
+      describe: 'The search term to filter notes by, will be applied to notes.content',
+      type: 'string' 
+    })
+  }, async(argv)=>{
+    const matches = await findNotes(argv.filter)
+    listNotes(matches)
+    })
+
+  .command('remove <id>', 'remove a note by id', yargs=>{
+    return yargs.positional('id', {
+      type:'number',
+      description: 'The id of the note you want to remove'
+    })
+  }, async(argv)=>{
+
+    })
+
+  .command('web [port]', 'launch website to see the notes', yargs=>{
+    return yargs 
+    .positional('port', {
+        describe: 'port to bind on',
+        default: 5000,
+        type: 'number'
+      })
+  }, async (argv) => {
+
+    })
+  .command('clean', 'remove all notes', ()=>{}, async(argv)=>{
+    
+  })
+  .demandCommand(1)
+  .parse() 
