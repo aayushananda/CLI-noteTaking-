3f6b956cb4ea96fce7d7bb7b79d8304e70ca2455
aayushananda <aayushanand300@gmail.com>
2026-03-29 19:27:47 +0530

    added web feature for notes

--- src/db.js
+++ src/db.js
@@ -1,20 +1,20 @@
-import fs from 'node:fs/promises'
+import fs from "node:fs/promises";
 
-const Db_path = new URL('../db.json', import.meta.url).pathname 
+const Db_path = new URL("../db.json", import.meta.url).pathname;
 
-export const getDB = async()=>{
-  const db = await fs.readFile(Db_path  , 'utf-8')
-  return JSON.parse(db)
-}
+export const getDB = async () => {
+  const db = await fs.readFile(Db_path, "utf-8");
+  return JSON.parse(db);
+};
 
-export const saveDB = async(db)=>{
- await fs.writeFile(Db_path, JSON.stringify(db, null, 2))
-return db 
-}
+export const saveDB = async (db) => {
+  await fs.writeFile(Db_path, JSON.stringify(db, null, 2));
+  return db;
+};
 
-export const insert = async(note)=>{
-  const db = await getDB()
-  db.notes.push(note)
-  await saveDB(db) 
-  return note
-}
+export const insertDB = async (note) => {
+  const db = await getDB();
+  db.notes.push(note);
+  await saveDB(db);
+  return note;
+};
